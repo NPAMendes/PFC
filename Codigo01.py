28557604b0273c8a95894b72cf324d7602291678
NPAMendes <npamendes@gmail.com>
2024-08-16 00:02:03 -0300

    Update and rename Validacao.py to Codigo01.py

--- Codigo01.py
+++ Codigo01.py
@@ -8,44 +8,52 @@ Coletando dados de arquivos CSV
 """
 
 # Coletando dados de arquivo CSV do Canal 1
-with open('F0001CH1.csv', 'r') as arquivo:
+with open('CH1_Inercia.csv', 'r') as arquivo:
     leitor = csv.reader(arquivo)
     i = list(leitor)
 
-# Extraindo dados de tensao
-tensao = np.zeros(len(i)) # Criando vetor que recebera os valores de tensao em Volts
-
-k = 0 # Inicializando um contador
+# Extraindo dados de tensao e tempo
+tensao = [] # Criando vetor que recebera os valores de tensao em Volts
+tempo = [] # Criando vetor que recebera os valores de tempo em Segundos
 
 # Criando loop para atribuicao de dados de tensao
 for a in i:
-    tensao[k] = float(a[4])
-    k=k+1
-    
-# Extraindo dasdos de tempo
-tempo = np.zeros(len(i)) # Criando vetor que recebera os valores de tempo em Segundos
-
-k = 0 # Inicializando um contador
-
-# Criando loop para atribuicao de dados de tempo
-for a in i:
-    tempo[k] = float(a[3])
-    k=k+1
+    tensao.append(float(a[4]))
+    tempo.append(float(a[3]))
 
 # Coletando dados de arquivo CSV do Canal 2
-with open('F0001CH2.csv', 'r') as arquivo:
+with open('CH2_Inercia.csv', 'r') as arquivo:
     leitor = csv.reader(arquivo)
     i = list(leitor)
 
 # Extraindo dasdos de corrente
-corrente = np.zeros(len(i)) # Criando vetor que recebera os valores de Corrente em Amperes
-
-k = 0 # Inicializando um contador
+corrente = [] # Criando vetor que recebera os valores de Corrente em Amperes
 
 # Criando loob para atribuicao de dados
 for a in i:
-    corrente[k] = float(a[4])/0.2
-    k=k+1
+    corrente.append(float(a[4])/0.2)
+    
+"""
+Processando dados
+"""
+# Definindo periï¿½odo de amostragem
+T = np.round(tempo[1]-tempo[0], 4)
+    
+# Estendendo os dados de tensao e corrente
+tensao2 = []
+corrente2 = []
+
+for k in range(27):
+    tensao2.append(tensao[-k-1])
+    corrente2.append(corrente[-k-1])
+
+for k in range(2000):
+    tensao = [*tensao, *tensao2]
+    corrente = [*corrente, *corrente2]
+
+# Estendendo os dados de tempo
+tempo = np.arange(tempo[0], len(tensao)*T + tempo[0], T)
+tempo = tempo[250:]
     
 """
 Criando modelo do sistema
@@ -56,42 +64,13 @@ Ra = 2.73 # Resistencia de Armadura
 La = 0.01638 # Indutancia de armadura
 b = 0.00704 # Coeficiente de viscosidade
 J = 0.0236 # Momento de inercia
-K = 1.356 # Constate de magnetização
+K = 1.356 # Constate de magnetizacao
 
-# Definindo peri­odo de amostragem
-T = np.round(tempo[1]-tempo[0], 4)
-    
-# Estendendo os dados de tensao
-tensao2 = np.zeros(27)
-
-for k in range(27):
-    tensao2[k] = tensao[-k-1]
-
-for k in range(2000):
-    tensao = [*tensao, *tensao2]
-
-# Estendendo os dados de tempo
-tempo2 = np.arange(tempo[0], len(tensao)*T + tempo[0], T)
-tempo = tempo2
-
-# Estendendo os dados de corrente
-corrente2 = np.zeros(27)
-
-for k in range(27):
-    corrente2[k] = corrente[-k-1]
-
-for k in range(2000):
-    corrente = [*corrente, *corrente2]
-
-# Criando vetor de tempo
-t = np.zeros(len(tempo)-250)
-t = tempo[250:]
-
-# Definindo a função de transferência
+# Definindo a funcao de transferencia
 G = ct.tf([K], [La*J, La*b + Ra*J, Ra*b + K**2])
 
 # Aplicando o sinal de entrada no sistema
-t, omega = ct.forced_response(G, t, tensao[250:])
+t, omega = ct.forced_response(G, tempo, tensao[250:])
 
 # convertendo omega em RPM
 omega = omega*30/np.pi
@@ -100,7 +79,7 @@ omega = omega*30/np.pi
 plt.figure(1)
 plt.subplot(311) 
 plt.plot(t,omega,'b', label = "Completo")
-plt.plot(t,0.95*omega[-1]*np.ones(len(t)),'--k', label = 'Critério de $2%$')
+plt.plot(t,0.95*omega[-1]*np.ones(len(t)),'--k', label = 'Criterio de $2%$')
 plt.grid()
 plt.ylabel('$\\omega (RPM)$')
 plt.xlim(0,0.3)
@@ -122,11 +101,11 @@ plt.xlim(0,0.35)
 Repetinndo o processo para sistema de primeira ordem com La = 0
 """
 
-# Definindo a função de transferência
+# Definindo a funcao de transferencia
 G1 = ct.tf([K], [Ra*J, Ra*b + K**2])
 
 # Aplicando o sinal de entrada no sistema
-tempo, omega1 = ct.forced_response(G1, t, tensao[250:])
+t, omega1 = ct.forced_response(G1, tempo, tensao[250:])
 
 # convertendo omega em RPM
 omega1 = omega1*30/np.pi
@@ -134,18 +113,18 @@ omega1 = omega1*30/np.pi
 #Plotando as varaveis de interesse
 plt.figure(1)
 plt.subplot(311) 
-plt.plot(tempo,omega1,'g', label = "$L_a = 0 H$")
+plt.plot(t,omega1,'g', label = "$L_a = 0 H$")
 
 
 """
 Repetinndo o processo para sistema de primeira ordem com La = 0 e b = 0
 """
 
-# Definindo a função de transferência
+# Definindo a funcao de transferencia
 G2 = ct.tf([K], [Ra*J, K**2])
 
 # Aplicando o sinal de entrada no sistema
-tempo, omega2 = ct.forced_response(G2, t, tensao[250:])
+t, omega2 = ct.forced_response(G2, tempo, tensao[250:])
 
 # convertendo omega em RPM
 omega2 = omega2*30/np.pi
@@ -167,26 +146,14 @@ plt.grid()
 Calculando parametros de desempenho
 """
 
-# Definindo variáveis de erro
+# Definindo variaveis de erro
 erro1 = (omega1[0:3000] - omega[0:3000])*np.pi/30
 erro2 = (omega2[0:3000] - omega[0:3000])*np.pi/30
 
-# Definindo variáveis do ISE
-ISE1 = 0
-ISE2 = 0
+# Definindo valores do IAE
+IAE1 = np.trapz(abs(erro1), dx = T)
+IAE2 = np.trapz(abs(erro2), dx = T)
 
-# Calculo do ISE
-for k in range(len(erro1)):
-    if k>0:
-        ISE1 = ISE1 + T*(erro1[k]**2 + erro1[k-1]**2)/2
-        ISE2 = ISE2 + T*(erro2[k]**2 + erro2[k-1]**2)/2
-
-# Definindo variáveis do ISE
-ITSE1 = 0
-ITSE2 = 0
-
-# Calculo do ITSE
-for k in range(len(erro1)):
-    if k>0:
-        ITSE1 = ITSE1 + T*k*(erro1[k]**2 + erro1[k-1]**2)/2
-        ITSE2 = ITSE2 + T*k*(erro2[k]**2 + erro2[k-1]**2)/2
+# Definindo valores do ITAE
+ITAE1 = np.trapz(abs(erro1)*tempo[0:3000], dx = T)
+ITAE2 = np.trapz(abs(erro2)*tempo[0:3000], dx = T)
