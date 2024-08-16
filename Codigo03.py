fa0a217946519e5f5fe4f329d80cd7c24eb1cb4b
NPAMendes <npamendes@gmail.com>
2024-08-16 00:46:41 -0300

    Add files via upload

---
+++ Codigo03.py
@@ -0,0 +1,46 @@
+# Importando bibliotecas necessárias
+import numpy as np
+import csv
+
+# Leitura do arquivo CSV
+with open('ControladorCampo.csv', 'r') as arquivo:
+    leitor = csv.reader(arquivo)
+    i = list(leitor)
+
+# Definiindo as variáveis de interesse
+time = []
+tensao_real = []
+tensao_desejada = []
+corrente_desejada = []
+corrente_real = []
+
+# Lendo as variáveis de interesse
+for a in i:
+    if a[0] != 'Time':
+        time.append(float(a[0]))
+        tensao_real.append(float(a[1]))
+        tensao_desejada.append(float(a[3]))
+        corrente_desejada.append(float(a[4]))
+        corrente_real.append(float(a[5]))
+
+# Covertndo as listas em vetores
+time = np.array(time)
+tensao_real = np.array(tensao_real)
+tensao_desejada = np.array(tensao_desejada)
+corrente_desejada = np.array(corrente_desejada)
+corrente_real = np.array(corrente_real)
+
+# Definindo variáveis de erro
+corrente = corrente_real - corrente_desejada
+tensao = tensao_real - tensao_desejada
+
+#Definindo período de amostragem
+T = time[1] - time[0]
+
+# Definindo valores do IAE
+IAEc = np.trapz(abs(corrente), dx = T)
+IAEt = np.trapz(abs(tensao), dx = T)
+
+# Definindo valores do ITAE
+ITAEc = np.trapz(abs(corrente)*time, dx = T)
+ITAEt = np.trapz(abs(tensao)*time, dx = T)
